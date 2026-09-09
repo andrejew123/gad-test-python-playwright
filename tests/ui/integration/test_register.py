771ad5509c855a3d46aeb03e0e7129f8ec7b5f36
andrejew123 <i.andrejewicz@gmail.com>
2026-09-09 13:24:51 +0200

    test: fix register test

--- tests/ui/integration/test_register.py
+++ tests/ui/integration/test_register.py
@@ -1,3 +1,5 @@
+import re
+
 from playwright.sync_api import expect
 
 from src.factories.user_factory import prepare_random_user
@@ -6,15 +8,15 @@ from src.factories.user_factory import prepare_random_user
 class TestRegister:
     def test_register_with_valid_data(self, register_page):
         # Arrange
-        expected_login_title = "Login"
+        expected_login_title = re.compile(r".*Login.*")
         register_user_data = prepare_random_user()
 
         # Act
         login_page = register_page.register(register_user_data)
-        title = login_page.get_title()
+        login_page.wait_for_page_to_load_url()
 
         # Assert
-        assert expected_login_title in title
+        expect(login_page.page).to_have_title(expected_login_title)
 
     def test_reject_register_with_invalid_email(self, register_page):
         # Arrange
